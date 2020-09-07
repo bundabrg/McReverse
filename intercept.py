5c3f148688aae8c056fbfa8fad80307dbe5df3c0
bundabrg <bundabrg@grieve.com.au>
2020-09-07 17:43:18 +0800

    stuff

--- intercept.py
+++ intercept.py
@@ -1,4 +1,10 @@
-import frida, sys
+import frida
 
 jscode = """
 """
+
+# Attach to Session Server using USB
+process = frida.get_usb_device().attach("com.mojang.minecraftpe")
+script = process.create_script(jscode)
+
+script.on('message')
